64f88ee59406c44e96efe5e669c3b27482c865ef
erenyeagervigi <blr.vignesh13006@gmail.com>
2025-03-07 15:58:53 +0530

    Update whileloop.py

--- whileloop.py
+++ whileloop.py
@@ -2,7 +2,7 @@
 while True:
     anime = input("enter the anime ")
     if anime == "":
-        print("lawde i said you to enter an anime")
+        print("mister!! i said you to enter an anime")
 
     else:
         print(f"{anime} is amazing")
