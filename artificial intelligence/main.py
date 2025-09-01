665627bfa832415edad33277b986d0e7384f41ef
erenyeagervigi <vignesh13006@gmail.com>
2025-09-01 10:44:38 +0530

    Update main.py

--- artificial intelligence/main.py
+++ artificial intelligence/main.py
@@ -4,11 +4,11 @@ import datetime
 
 time = datetime.datetime.now()
 
-API_ID = "77f81f14"
-API_KEY = "c7476e9d19ce4ab5b1154c8a707a7350"
+API_ID = api_id
+API_KEY = api_key
 
 url = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheety_url = 'https://api.sheety.co/6080f6c2e197e7a78fdb09d4091eff6a/workoutTracking/sheet1'
+sheety_url = sheety_url
 headers = {
     "x-app-id": API_ID,
     "x-app-key": API_KEY,
@@ -25,7 +25,7 @@ recieved = response.json()
 print(recieved)
 
 sheety_headers = {
-    "Authorization": "Bearer erenyeager",
+    "Authorization": "Bearer username",
     "Content-Type" :"application/json"
 }
 for exersice in recieved['exercises']:
@@ -39,4 +39,5 @@ for exersice in recieved['exercises']:
         }
     }
     response1 = requests.post(url = sheety_url,json=parameters, headers= sheety_headers)
+
     print(response1.text)
